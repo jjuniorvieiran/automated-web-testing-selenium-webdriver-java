7851a43c367239b88a8dde09b7ff18d95ba7dfee
Junior <junior.jaava@gmail.com>
2020-10-29 18:18:49 +0100

    Selenium and Checkboxes

--- 05/demos/m4/after/src/main/java/com/pluralsight/WebDriverCheckboxes.java
+++ 05/demos/m4/after/src/main/java/com/pluralsight/WebDriverCheckboxes.java
@@ -4,12 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverCheckboxes {
 
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
-		driver.get("file:///C:/dev/workspace/WebDriverDemo/src/main/webapp/CheckboxTest.html");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///Users/josejunior/dev/git/automated-web-testing-selenium-webdriver-java/05/demos/m4/after/src/main/webapp/CheckboxTest.html");
 		
 		WebElement checkbox = driver.findElement(By.id("lettuceCheckbox"));
 		
