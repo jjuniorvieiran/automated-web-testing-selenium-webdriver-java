14a2a2f5219fed4200b8146edeb26a6d4c43a327
Junior <junior.jaava@gmail.com>
2020-10-29 18:23:16 +0100

    Selenium and Select Item

--- 05/demos/m4/after/src/main/java/com/pluralsight/WebDriverSelectItems.java
+++ 05/demos/m4/after/src/main/java/com/pluralsight/WebDriverSelectItems.java
@@ -3,14 +3,14 @@ package com.pluralsight;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class WebDriverSelectItems {
 
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
-		driver.get("file:///C:/dev/workspace/WebDriverDemo/src/main/webapp/SelectItemTest.html");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///Users/josejunior/dev/git/automated-web-testing-selenium-webdriver-java/05/demos/m4/after/src/main/webapp/SelectItemTest.html");
 		
 		WebElement selectElement = driver.findElement(By.id("select1"));
 		Select select = new Select(selectElement);
